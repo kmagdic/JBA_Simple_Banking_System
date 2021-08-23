6185e5dff20530a175b8c12583e0555ec21c7fe4
Karlo Magdic <karlo.magdic@gmail.com>
2021-08-23 15:40:35 +0200

    Initial commit

---
+++ Simple Banking System/task/src/banking/Account.java
@@ -0,0 +1,45 @@
+package banking;
+
+class Account {
+    private String cardNumber;
+    private String pin;
+    private int balance;
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Account() {
+        cardNumber = "400000" + String.format("%010d", (long) (Math.random() * 9999999999L)) ;
+        pin = String.format("%04d", (long) (Math.random() * 9999));
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", pin='" + pin + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
---
+++ Simple Banking System/task/src/banking/Main.java
@@ -0,0 +1,96 @@
+package banking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static List<Account> accountList = new ArrayList<Account>();
+
+    public static Account findAccount(String cardNumber, String pin) {
+        for(Account a : accountList) {
+            if(a.getCardNumber().equals(cardNumber) && a.getPin().equals(pin))
+                return a;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean doExit = false;
+        Account currAccount = null;
+        int menu = -1;
+
+        do {
+            if (currAccount == null) {
+                System.out.println("\n1. Create an account");
+                System.out.println("2. Log into account");
+                System.out.println("0. Exit");
+                System.out.print(">");
+
+                menu = scanner.nextInt();
+
+                // create an account
+                if(menu == 1) {
+                    Account newAccount = new Account();
+                    accountList.add(newAccount);
+                    System.out.println("Your card has been created.");
+                    System.out.println("Your card number:\n" + newAccount.getCardNumber());
+                    System.out.println("Your card PIN:\n" + newAccount.getPin());
+               }
+                // log into account
+                else if (menu == 2) {
+                    System.out.print("Enter your card number:\n>");
+                    String cardNumber = scanner.next();
+
+                    System.out.print("Enter your pin:\n>");
+                    String pin = scanner.next();
+
+                    currAccount = findAccount(cardNumber, pin);
+                    if (currAccount == null) {
+                        System.out.println("Wrong card number or PIN");
+                    } else  {
+                        System.out.println("You have successfully logged in!");
+                    }
+                }
+
+
+            }
+            // logged in user
+            else {
+                System.out.println("\n1. Balance");
+                System.out.println("2. Log out");
+                System.out.println("3. Set balance");
+                System.out.println("0. Exit");
+                System.out.print(">");
+
+                int menu2 = scanner.nextInt();
+
+                if (menu2 == 1) {
+                    System.out.println("Balance: " + currAccount.getBalance());
+                } else if (menu2 == 2) {
+                    currAccount = null;
+                    System.out.println("You have successfully logged out!");
+                } else if (menu2 == 3) {
+                   try {
+                       System.out.print("Enter new balance\n>");
+                       int b = scanner.nextInt();
+                       currAccount.setBalance(b);
+                       System.out.println("New balance is set!");
+                   } catch (ArithmeticException e) {
+                       System.out.println("Wrong number!");
+                   }
+                }
+
+
+            }
+
+
+
+        } while(menu != 0);
+
+        System.out.println("Bye!");
+    }
+}
